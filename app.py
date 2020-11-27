e63bf61602741811e24a6b4180e34f416d442021
JunChoi <cjy13753@gmail.com>
2020-11-28 01:16:03 +0900

    Enable dynamic change for hostname in index.html

--- app.py
+++ app.py
@@ -31,6 +31,5 @@ api.add_resource(WorkoutRecordList, '/workoutrecords')
 api.add_resource(WorkoutRecord, '/workout_record')
 api.add_resource(WorkoutRecordTable, '/workout_record_table')
 
-
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
